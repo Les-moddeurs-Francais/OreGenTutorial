aa39cb6f63f202928376c2b5525bc8bae7aa9060
Mysterious_Dev <mysteriousdevs@gmail.com>
2020-10-28 14:07:17 +0100

    Update Mappings

--- src/main/java/fr/lmf/oregentuto/init/ModFeatures.java
+++ src/main/java/fr/lmf/oregentuto/init/ModFeatures.java
@@ -9,7 +9,7 @@ import net.minecraft.world.gen.feature.*;
 
 public class ModFeatures {
 
-    public static final ConfiguredFeature<?, ?> DIAMOND_BLOCK = register("diamond_block", (ConfiguredFeature)((ConfiguredFeature)((ConfiguredFeature)Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, Blocks.DIAMOND_BLOCK.getDefaultState(), 8)).func_242733_d(128)).func_242728_a()).func_242731_b(20));
+    public static final ConfiguredFeature<?, ?> DIAMOND_BLOCK = register("diamond_block", (ConfiguredFeature)((ConfiguredFeature)((ConfiguredFeature)Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, Blocks.DIAMOND_BLOCK.getDefaultState(), 8)).maxHeight(128)).func_242728_a()).repeat(20));
 
     private static <FC extends IFeatureConfig> ConfiguredFeature<FC, ?> register(String id, ConfiguredFeature<FC, ?> feature) {
         return (ConfiguredFeature) Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation(Main.MODID, id), feature);
