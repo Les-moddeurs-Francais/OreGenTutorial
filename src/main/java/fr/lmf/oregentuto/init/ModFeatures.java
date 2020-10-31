e5346d47e2ea96f8793ef0a227214b45614869e5
Mysterious_Dev <mysteriousdevs@gmail.com>
2020-10-31 16:40:02 +0100

    Update Mappings and delete cast

--- src/main/java/fr/lmf/oregentuto/init/ModFeatures.java
+++ src/main/java/fr/lmf/oregentuto/init/ModFeatures.java
@@ -9,7 +9,7 @@ import net.minecraft.world.gen.feature.*;
 
 public class ModFeatures {
 
-    public static final ConfiguredFeature<?, ?> DIAMOND_BLOCK = register("diamond_block", (ConfiguredFeature)((ConfiguredFeature)((ConfiguredFeature)Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, Blocks.DIAMOND_BLOCK.getDefaultState(), 8)).maxHeight(128)).func_242728_a()).repeat(20));
+    public static final ConfiguredFeature<?, ?> DIAMOND_BLOCK = register("diamond_block", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, Blocks.DIAMOND_BLOCK.getDefaultState(), 8)).maxHeight(128)).func_242728_a().repeat(20);
 
     private static <FC extends IFeatureConfig> ConfiguredFeature<FC, ?> register(String id, ConfiguredFeature<FC, ?> feature) {
         return (ConfiguredFeature) Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation(Main.MODID, id), feature);
